b6d266a1388d79c6938082fac9f3ef47ed7ee71f
Noah <noah.rutkovsky@gmail.com>
2020-04-05 14:46:13 -0400

    successfully writes to database!

--- app.js
+++ app.js
@@ -1,27 +1,29 @@
 const express = require('express');
 const mysql = require('mysql');
 const app = express();
-var connection = mysql.createConnection({
-    host : 'localhost',
-    user: 'root',
-    password: 'root',
-    database: 'mydb'
-});
+
 
 app.use(express.static('public'));
 
 app.post('/*', function(req, res){
     console.log('post received');
 
+    var connection = mysql.createConnection({
+        host : 'localhost',
+        user: 'root',
+        password: 'root',
+        database: 'mydb'
+    });
+
     connection.connect(function(err){
         if (err) throw err;
         console.log('Connected');
-    })
-//(id, email, message, time, parent)
+    });
+    
     connection.query('INSERT INTO messages VALUES ('+req.header('id')+', \"'+req.header('email')+'\", \"'+req.header('message')+'\", '+req.header('time')+', '+req.header('parent')+');', function(err){
         if (err) throw err;
     })
-
+    
     connection.end();
 
 })
